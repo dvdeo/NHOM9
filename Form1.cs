3abec11db897dfc5304455c0088c664bc1df435d
Nero <dvdeo@outlook.com.vn>
2023-11-01 17:00:05 +0700

    add new button

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace NHOM9
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
     }
 }
